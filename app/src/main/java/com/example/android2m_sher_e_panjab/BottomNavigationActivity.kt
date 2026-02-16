004fe0570fd55cdc49da0d1a3c9fbfa191612022
kaurpalak-star <kaurpalak1618@gmail.com>
2026-02-16 15:56:28 +0530

    16/02

--- app/src/main/java/com/example/android2m_sher_e_panjab/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/android2m_sher_e_panjab/BottomNavigationActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomNavigationActivity : AppCompatActivity() {
+    private lateinit var upWithNavController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,10 @@ class BottomNavigationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.nav_view)
+        val fragmentContainer = findNavController(R.id.fragment)
+
+        bottomNavigation.setupWithNavController(fragmentContainer)
     }
 }
