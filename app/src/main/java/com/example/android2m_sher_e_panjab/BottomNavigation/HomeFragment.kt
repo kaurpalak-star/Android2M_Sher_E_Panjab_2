1045133232e2b16504ae50640301aea6d43240d1
jatino7 <jatin@o7tec.com>
2026-03-16 06:36:14 +0530

    residual code removed

--- app/src/main/java/com/example/android2m_sher_e_panjab/BottomNavigation/HomeFragment.kt
+++ app/src/main/java/com/example/android2m_sher_e_panjab/BottomNavigation/HomeFragment.kt
@@ -14,16 +14,6 @@ import com.example.android2m_sher_e_panjab.com.example.android2m_sher_e_panjab.I
 import com.example.android2m_sher_e_panjab.com.example.android2m_sher_e_panjab.ItemAdapter
 import com.example.android2m_sher_e_panjab.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment(), ItemAdapter.OnItemClick, HomeAdapter.OnItemClick {
 
     private lateinit var binding: FragmentHomeBinding
